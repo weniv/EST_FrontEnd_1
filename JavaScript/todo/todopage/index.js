39e112f91db51bac15a8b53b6f3fdc5098a17496
WonBeomChoi <cwb2310@gmail.com>
2024-07-24 15:33:50 +0900

    투두 추가 기능 만들기

---
+++ JavaScript/todo/todopage/index.js
@@ -0,0 +1,36 @@
+const $input = document.querySelector("input");
+const $button = document.querySelector("button");
+const $ul = document.querySelector("ul");
+const $form = document.querySelector("form");
+
+const fetchTodos = async function(){
+    const res = await fetch("http://localhost:3000/todos");
+    const json = await res.json();
+    json.forEach((item)=>{
+        const $li = document.createElement("li");
+        $li.textContent = item.todo;
+        $ul.appendChild($li);
+    })
+}
+fetchTodos()
+
+const addTodo = async function(todoTxt){
+    const req = await fetch("http://localhost:3000/todos",{
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body:JSON.stringify({todo:todoTxt, done:false})
+    })
+    const newPost = await req.json()
+    const $li = document.createElement("li");
+    $li.textContent = newPost.todo;
+    $ul.appendChild($li);
+}
+
+$form.addEventListener("submit",async function(e){
+    e.preventDefault();
+    const todoTxt = $input.value;
+    await addTodo(todoTxt);
+    $input.value = ""
+})
