03a9ed50e6af14e27c27bae2b65268b879eee312
WonBeomChoi <cwb2310@gmail.com>
2024-07-25 11:02:03 +0900

    삭제기능

--- JavaScript/todo/todopage/index.js
+++ JavaScript/todo/todopage/index.js
@@ -1,3 +1,6 @@
+const url = "http://localhost:3000/todos"
+
+
 const $input = document.querySelector("input");
 const $button = document.querySelector("button");
 const $ul = document.querySelector("ul");
@@ -7,9 +10,10 @@ const $form = document.querySelector("form");
 // todo ui에는 내용표시, 삭제버튼, 체크, 수정하기
 const createTodoUi = (todoData)=>{
     const $li = document.createElement("li");
-    
+    $li.id = todoData.id
     const $delButton = document.createElement("button");
     $delButton.textContent ="삭제하기";
+    $delButton.classList.add("delete-btn");
 
     const $editButton = document.createElement("button");
     $editButton.textContent = "수정하기";
@@ -19,13 +23,13 @@ const createTodoUi = (todoData)=>{
 
     $li.textContent = todoData.todo;
     $ul.appendChild($li);
-    $ul.appendChild($delButton);
-    $ul.appendChild($editButton);
-    $ul.appendChild($checkBox);
+    $li.appendChild($delButton);
+    $li.appendChild($editButton);
+    $li.appendChild($checkBox);
 }
 
 const fetchTodos = async function(){
-    const res = await fetch("http://localhost:3000/todos");
+    const res = await fetch(url);
     const json = await res.json();
     json.forEach((todoData)=>{
         createTodoUi(todoData);
@@ -35,7 +39,7 @@ fetchTodos()
 
 const addTodo = async function(todoTxt){
     try {
-        const req = await fetch("http://localhost:3000/todos",{
+        const req = await fetch(url,{
             method:"POST",
             headers:{
                 "Content-Type":"application/json"
@@ -52,8 +56,6 @@ const addTodo = async function(todoTxt){
     }
 }
 
-
-
 $form.addEventListener("submit",async function(e){
     e.preventDefault();
     const todoTxt = $input.value;
@@ -64,6 +66,25 @@ $form.addEventListener("submit",async function(e){
 
 // TODO:: 완료하기 기능이랑 todo삭제하기 기능해야됩니다. + 수정하기도 해야됩니다.
 
-const deleteTodo = ()=>{
-
+const deleteTodo = async (id)=>{
+    try {
+        const res = await fetch(`http://localhost:3000/todos/${id}`,{
+            "method":"DELETE"
+        })
+        return res.status === 200
+    } catch (error) {
+           //예외처리 하면 좋음
+    }
 }
+
+$ul.addEventListener("click", async (e)=>{
+    if([...e.target.classList].includes("delete-btn")){
+        const parentNode = e.target.parentNode
+        const isDelete = await deleteTodo(parentNode.id)
+        if (isDelete) {
+            parentNode.remove();
+        }else{
+            alert("잘못된 요청입니다.");
+        }
+    }
+})
