68afd070d3e577fd9462dab089e46cadd1417763
nugurejeil <nugurejeil1@gmail.com>
2024-08-22 11:53:04 +0900

    1만시간의법칙 리액트 기능구현중

--- react/240822/10000react/src/App.js
+++ react/240822/10000react/src/App.js
@@ -3,11 +3,14 @@ import Header from "./components/Header/Header";
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import Modal from './components/Modal/Modal';
+import { useState } from 'react';
 
 function App() {
+  const [someVal, setSomeVal] = useState(0);
+
   return (
     <div id="app">
-      <Header />
+      <Header someVal={someVal} />
       <Main />
       <Footer />
       <Modal />
--- react/240822/10000react/src/components/Header/Header.jsx
+++ react/240822/10000react/src/components/Header/Header.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 import title from '../../images-law/title.png';
 import subtitle from '../../images-law/txt_subtitle.png';
-import './Header.css';
+// import './Header.module.css';
+import styles from './Header.module.css';
 
-export default function Header() {
+export default function Header(props) {
     return (
         <header>
-            <h1 className="tit-law">
-                <img src={title} alt="1만 시간의 법칙" className="img-tit" />
+            <h1 className={styles['tit-law']}>
+                <img src={title} alt="1만 시간의 법칙" className={styles["img-tit"]} />
             </h1>
-            <img src={subtitle} alt="연습은 어제의 당신보다 당신을 더 낫게 만든다." className="img-txt" />
-            <p className="txt-desc">
-                <strong className="tit-desc">1만 시간의 법칙</strong>은<br />
+            <img src={subtitle} alt="연습은 어제의 당신보다 당신을 더 낫게 만든다." className={styles["img-txt"]} />
+            <p className={styles["txt-desc"]} val={props}>
+                <strong className={styles["tit-desc"]}>1만 시간의 법칙</strong>은<br />
                 어떤 분야의 전문가가 되기 위해서는<br />
                 최소한 1만 시간의 훈련이 필요하다는 법칙이다.
             </p>
