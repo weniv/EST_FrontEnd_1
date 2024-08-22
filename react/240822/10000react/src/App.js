4a71cbf62b5f6872a94349562c2f7cc47b18690d
nugurejeil <nugurejeil1@gmail.com>
2024-08-22 10:30:34 +0900

    1만시간의법칙 업로드

---
+++ react/240822/10000react/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div id="app">
+      <Header />
+    </div>
+  );
+}
+export default App;
