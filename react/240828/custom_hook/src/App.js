8cf63ce04ec140e476ca709007b80735566f96f6
nugurejeil <nugurejeil1@gmail.com>
2024-08-28 11:18:00 +0900

    custom훅 교안 내용 까지 실습

---
+++ react/240828/custom_hook/src/App.js
@@ -0,0 +1,12 @@
+import InputComponent from "./InputComponent";
+import SomethingComponent from "./SomethingComponent";
+
+function App() {
+  return (
+    <div>
+      <InputComponent />
+      <SomethingComponent />
+    </div>
+  );
+}
+export default App;
