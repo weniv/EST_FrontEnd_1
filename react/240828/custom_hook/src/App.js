ec7a59c8a06117a44563b8152e26e23db93e7d29
nugurejeil <nugurejeil1@gmail.com>
2024-08-29 11:50:14 +0900

    무한 스크롤 데이터 통신 완료

--- react/240828/custom_hook/src/App.js
+++ react/240828/custom_hook/src/App.js
@@ -3,32 +3,41 @@ import SomethingComponent from "./SomethingComponent";
 import useScroll from "./Hook/useScroll";
 import { useEffect, useState } from "react";
 import ImageList from "./Components/ImageList";
+import Loading from "./Components/Loading";
 
 function App() {
 
   const [imageList, setImageList] = useState([]);
 
+  const [isLoading, setIsLoading] = useState(false);
+
   async function fetchImages() {
-    const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
+    setIsLoading(true);
+    try {
+      const response = await fetch('https://picsum.photos/v2/list?page=1&limit=5');
 
-    if (!response.ok) {
-      throw new Error();
+      if (!response.ok) {
+        throw new Error();
+      }
+
+      const datas = await response.json();
+
+      setImageList(datas);
+      setIsLoading(false);
+
+    } catch (error) {
+      console.error('네트워크 통신에 문제가 있습니다.', error);
+      setIsLoading(false);
     }
-
-    const datas = await response.json();
-
-    setImageList(datas);
   }
 
   useEffect(() => {
     fetchImages();
   }, []);
 
-  console.log(imageList);
-
   return (
-    <div style={{ height: '2000px' }
-    }>
+    <div style={{ height: '2000px' }}>
+      {isLoading && <Loading />}
       <ImageList imageList={imageList} />
     </div >
   );
