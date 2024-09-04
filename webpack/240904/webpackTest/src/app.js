0b9a30680aa64263c580752b4fc859d91ae53b8d
nugurejeil <nugurejeil1@gmail.com>
2024-09-04 11:55:22 +0900

    웹팩 관련 코드 업로드

---
+++ webpack/240904/webpackTest/src/app.js
@@ -0,0 +1,9 @@
+import plus from "./plus.js";
+import './styles.css';
+// const style = require('./styles.css');
+
+console.log(plus(10, 5));
+
+function minus(a, b) {
+    return a - b;
+}
---
+++ webpack/240904/webpackTest/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+
+    entry: {
+        main: path.resolve('./src/app.js')
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.resolve('./dist')
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    path.resolve('./myLoader.js')
+                ]
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    }
+}
