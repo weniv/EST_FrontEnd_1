ab23ab6e8d85d0450253863bca6650b1014a2cbc
nugurejeil <nugurejeil1@gmail.com>
2024-09-05 16:53:18 +0900

    9/5일 코드 업로드

--- webpack/240904/webpackTest/src/app.js
+++ webpack/240904/webpackTest/src/app.js
@@ -1,8 +1,20 @@
 import plus from "./plus.js";
 import './styles.css';
 // const style = require('./styles.css');
+import rabbit from './images.png';
 
-console.log(plus(10, 5));
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.body.innerHTML = `<img src=${rabbit}>`;
+// });
+
+let env;
+console.log(process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+    env = dev;
+} else {
+    env = pro;
+}
+console.log(env);
 
 function minus(a, b) {
     return a - b;
