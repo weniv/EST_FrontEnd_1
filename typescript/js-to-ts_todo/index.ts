f22d29d53dacf8e4471fa1a340f36d7685c57f7f
WonBeomChoi <cwb2310@gmail.com>
2024-09-10 14:18:28 +0900

    any로 빨간줄 없애보기(타입주기)

--- typescript/js-to-ts_todo/index.ts
+++ typescript/js-to-ts_todo/index.ts
@@ -9,7 +9,7 @@ const todoDatas = [
     },
 ]
 
-const addTodoData = (todoText) =>{
+const addTodoData = (todoText:any) =>{
     const newTodoId = todoDatas[todoDatas.length-1].id + 1
     const newTodo = {
         id:newTodoId,
@@ -20,21 +20,21 @@ const addTodoData = (todoText) =>{
 }
 
 const addTodoList = () =>{
-    const $todoInput = document.querySelector("#todo-input");
+    const $todoInput:any = document.querySelector("#todo-input");
     const todoText = $todoInput.value;
     $todoInput.value = "";
     const todoDatas = addTodoData(todoText);
     todoListRender(todoDatas);
 }
 
-const $todoInputButton = document.querySelector("#todo-button");
+const $todoInputButton:any = document.querySelector("#todo-button");
 $todoInputButton.addEventListener("click",addTodoList);
 
 // 데이터를 넣어주면 todoList를 만들어주는애
-const todoListRender = (todoDatas) =>{
-    const $todoContainer = document.querySelector("#todo-container");
+const todoListRender = (todoDatas:any) =>{
+    const $todoContainer:any = document.querySelector("#todo-container");
     $todoContainer.innerHTML = "";
-    todoDatas.forEach(todoData => {
+    todoDatas.forEach((todoData:any) => {
         const $todoLi = document.createElement("li");
         const $todoP = document.createElement("p");
         $todoP.innerText = todoData.todo;
