a0866b4d0210952e6baaa63975821ad6229ae430
WonBeomChoi <cwb2310@gmail.com>
2024-09-12 13:51:56 +0900

    중간커밋

---
+++ typescript/react/ts-react/src/components/Todo.tsx
@@ -0,0 +1,21 @@
+import { useState,ChangeEvent } from "react";
+import TodoView from "./TodoView";
+import useTodoData from "../hooks/todoHook";
+
+
+const Todo = () => {
+  const [todoText, setTodoText] = useState<string>("");
+  const [todoList, postTodo, isLoading] = useTodoData();
+  const handleInput = (e:ChangeEvent) => {
+    const newTodoText = e.target.value;
+    setTodoText(newTodoText);
+  };
+  const handleButton = () => {
+    postTodo(todoText);
+  };
+
+  const props = { todoList, handleInput, todoText, handleButton };
+  return isLoading ? <h1>로딩중 페이지</h1> : <TodoView {...props} />;
+};
+
+export default Todo;
