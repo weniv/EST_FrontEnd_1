8a68100bec5fc9635f6c27ac3bdd8fc28a274b44
WonBeomChoi <cwb2310@gmail.com>
2024-09-12 09:12:33 +0900

    투두 추가하기 기능

--- typescript/react/todo/src/App.jsx
+++ typescript/react/todo/src/App.jsx
@@ -1,12 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Todo(props) {
-  const [todoList, setTodolist] = useState([
+  const [todoList, setTodoList] = useState([
     { id: "1", todo: "아침먹기" },
     { id: "2", todo: "점심먹기" },
   ]);
+
+  const [todoText, setTodoText] = useState("");
+  const handleInput = (e) => {
+    const newTodoText = e.target.value;
+    setTodoText(newTodoText);
+  };
+
+  const handleButton = () => {
+    setTodoList((prevTodoList) => {
+      const newTodo = {
+        id: +todoList[todoList.length - 1].id + 1,
+        todo: todoText,
+      };
+      const newTodoList = [...prevTodoList, newTodo];
+      if (newTodoList.length > 5) {
+        newTodoList.shift();
+      }
+      return newTodoList;
+    });
+  };
 
   return (
     <div>
@@ -18,8 +38,10 @@ function Todo(props) {
           </li>
         ))}
       </ul>
-      <input type="text" />
-      <button type="button">투두추가</button>
+      <input type="text" value={todoText} onChange={handleInput} />
+      <button type="button" onClick={handleButton}>
+        투두추가
+      </button>
     </div>
   );
 }
